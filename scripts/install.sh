926c213c32496959c672c4d9523dae95b3d46950
Andy Attebery <me@andyattebery.com>
2017-09-01 21:36:15 -0500

    updated install to download warden script and setting env variables in bash/zsh profile

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,25 +1,48 @@
 #!/usr/bin/env sh
 
-set_env_variables()
+get_shell_profile_path()
 {
-    export WARDEN_VERSION="0.1.0-alpha.1"
-    export WARDEN_OS_ARCH="darwin-amd64"
-    if [[ -z "$WARDEN_HOME" ]]
-        export WARDEN_HOME="$HOME/.warden"
-    fi
+    shell=$(echo $SHELL | grep -Eo "\w+$")
+
+    case $shell in
+    "zsh")
+        echo "$HOME/.zprofile"
+        ;;
+    "bash")
+        echo "$HOME/.bash_profile"
+        ;;
+    esac
 }
 
-create_warden_home() 
+set_env_variables()
 {
-    if [ ! -d "$WARDEN_HOME" ]; then
-        mkdir -p $WARDEN_HOME
+    cat >> "$(get_shell_profile_path)" <<- EOM
+export WARDEN_VERSION="0.1.0-alpha.1"
+export WARDEN_OS_ARCH="darwin-amd64"
+export WARDEN_HOME="$HOME/.warden"
+EOM
+
+    export WARDEN_VERSION="0.1.0-alpha.1"
+    export WARDEN_OS_ARCH="darwin-amd64"
+    export WARDEN_HOME="$HOME/.warden"
+}
+
+create_warden_directories() 
+{
+    if [[ ! -d "$WARDEN_HOME" ]] || [[ ! -d "$WARDEN_HOME/bin" ]]
+    then
+        mkdir -p "$WARDEN_HOME/bin"
     fi
 }
 
 download_warden_script()
 {
-    curl -L -o "$WARDEN_HOME"
+    warden_script_path="$WARDEN_HOME/bin/warden"
+    curl -fsSL "https://raw.githubusercontent.com/NoFateLLC/warden-releases/master/scripts/warden.sh" > "$warden_script_path"
+    chmod +x "$warden_script_path"
+    exec "$warden_script_path"
 }
 
 set_env_variables
-create_warden_home
+create_warden_directories
+download_warden_script
